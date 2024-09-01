fa041781af6165580f04d054c12d74848099f7b0
omnandurkar <nandurkarom172@gmail.com>
2024-09-01 23:24:46 +0530

    ref: aaded pulse

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,34 +3,46 @@ import { FaRegFileAlt } from 'react-icons/fa'
 import { IoClose } from 'react-icons/io5'
 import { LuDownload } from 'react-icons/lu'
 import { motion } from 'framer-motion'
+import { CircleX } from 'lucide-react'
 
-const Card = ({ data, reference }) => {
-    return (
-        <motion.div
-            drag dragConstraints={reference}
-            whileDrag={{ scale: 1.1 }}
-            dragElastic={0.2}
-            dragTransition={{ bounceStiffness: 500, bounceDamping: 10 }}
-            className=' flex-shrink-0 relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden' >
-            <FaRegFileAlt />
-            <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
-            <div className='footer  absolute bottom-0  w-full left-0 '>
-                <div className='flex justify-between px-8 py-3 items-center mb-5'>
-                    <h5>{data.filesize}</h5>
-                    <span className='w-7 h-7 bg-zinc-600 rounded-full  flex items-center justify-center '>
-                        {
-                            data.close ? <IoClose /> : <LuDownload />
-                        }
-                    </span>
+const
+    Card = ({ data, reference }) => {
+
+        const handleDelete = () => {
+            console.log('delete')
+        }
+
+        return (
+            <motion.div
+                drag dragConstraints={reference}
+                whileDrag={{ scale: 1.1 }}
+                dragElastic={0.2}
+                dragTransition={{ bounceStiffness: 500, bounceDamping: 10 }}
+                className=' flex-shrink-0 relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden' >
+                <div className='flex justify-between'>
+                    <FaRegFileAlt />
+                    <CircleX onClick={handleDelete} size={20} /> </div>
+                <p className='text-md capitalize leading-tight mt-5 font-bold'>{data.title}</p>
+                <div className='overflow-y-auto'>
+                    <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
                 </div>
-                {
-                    data.tag.isOpen ? <div className={`tag flex justify-center items-center w-full py-4 ${"bg-"+ data.tag.tagColor + "-600"}`} >
-                        <h2 className='font-semibold text-sm'>{data.tag.tagTitle}</h2>
-                    </div> : null
-                }
-            </div>
-        </motion.div>
-    )
-}
+                <div className='footer  absolute bottom-0  w-full left-0 '>
+                    <div className='flex justify-between px-8 py-3 items-center mb-5'>
+                        <h5>{data.filesize}</h5>
+                        <span className='w-7 h-7 bg-zinc-600 rounded-full  flex items-center justify-center '>
+                            {
+                                data.close ? <IoClose /> : <LuDownload />
+                            }
+                        </span>
+                    </div>
+                    {
+                        data.tag.isOpen ? <div className={`tag flex justify-center items-center w-full py-4  ${data.tag.tagColor === "green" ? 'bg-green-600' : 'bg-blue-600'} `} >
+                            <h2 className='font-semibold text-sm'>{data.tag.tagTitle}</h2>
+                        </div> : null
+                    }
+                </div>
+            </motion.div>
+        )
+    }
 
 export default Card
--- src/components/Grounds/Foreground.jsx
+++ src/components/Grounds/Foreground.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Card from '../Card'
 import { FetchNoteAction } from '@/actions/page'
 import AddNote from '../AddNote'
+import { set } from 'mongoose'
 // import AddNote from '../AddNote'
 // import Card from './Card'
 
@@ -13,6 +14,7 @@ const Foreground = () => {
     const ref = useRef(null)
 
     const [notes, setNotes] = useState([]);
+    const [notesIsLoading, setNotesIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNotes = async () => {
@@ -20,21 +22,62 @@ const Foreground = () => {
             setNotes(fetchedNotes);
         };
         fetchNotes();
+        setNotesIsLoading(false);
     }, []);
 
 
     return (
-        <div ref={ref} className='fixed p-5 top-0 left-0 w-full h-full  z-[3] flex gap-10  flex-wrap'>
+        <div ref={ref} className='fixed p-5 top-0 left-0 w-full h-full  z-[3] '>
 
-            {
-                notes.map((note, index) => {
-                    return (
-                        <Card key={index} data={note} reference={ref} />
-                    )
-                })
-            }
+            <div className=' mt-10'>
 
-            <AddNote />
+                {
+                    notesIsLoading ?
+                        <div className='flex gap-10  flex-wrap mx-auto'>
+
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+                            <div className='w-60 h-72 rounded-[50px] bg-neutral-900/50 animate-pulse'>
+                            </div>
+
+
+                        </div>
+                        :
+                        <div className='flex gap-10  flex-wrap'>
+
+                            {
+                                notes.map((note, index) => {
+                                    return (
+                                        <Card key={index} data={note} reference={ref} />
+                                    )
+                                })
+                            }
+                        </div>
+                }
+
+
+            </div>
+
+            <div className="absolute top-5 right-5">
+                <AddNote />
+            </div>
+
 
 
 
